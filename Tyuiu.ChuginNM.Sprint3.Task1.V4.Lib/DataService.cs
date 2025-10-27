e1da1be889eda0c03b3382259ea03b1aca93bf0a
CDOOR <CDOOR@CHUGIN>
2025-10-27 17:52:50 +0500

    Task1

---
+++ Tyuiu.ChuginNM.Sprint3.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ChuginNM.Sprint3.Task1.V4.Lib
+{
+    public class DataService : ISprint3Task1V4
+    {
+        public double GetSumSeries(int startValue, int stopValue)
+        {
+
+            double S = 0.0;
+
+            for (int k = startValue; k <= stopValue; k++)
+            {
+                S += 1.0 / (k * k);
+                
+            }
+
+            return Math.Round(S, 3);
+        }
+    }
+}
---
+++ Tyuiu.ChuginNM.Sprint3.Task1.V4.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ChuginNM.Sprint3.Task1.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+        }
+    }
+}
