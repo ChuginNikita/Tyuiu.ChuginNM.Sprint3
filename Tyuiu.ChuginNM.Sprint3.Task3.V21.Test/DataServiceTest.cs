a638d212635fa5776890c9c55ca3a770e1cdbb1c
CDOOR <CDOOR@CHUGIN>
2025-10-28 19:27:52 +0500

    Task3

---
+++ Tyuiu.ChuginNM.Sprint3.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.ChuginNM.Sprint3.Task3.V21.Lib;
+
+namespace Tyuiu.ChuginNM.Sprint3.Task3.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+        }
+    }
+}
--- Tyuiu.ChuginNM.Sprint3.Task3.V21/Program.cs
+++ Tyuiu.ChuginNM.Sprint3.Task3.V21/Program.cs
@@ -1,33 +1,34 @@
-﻿using Tyuiu.ChuginNM.Sprint3.Task2.V17.Lib;
+﻿using Tyuiu.ChuginNM.Sprint3.Task3.V21.Lib;
 
-namespace Tyuiu.ChuginNM.Sprint3.Task2.V17
+namespace Tyuiu.ChuginNM.Sprint3.Task3.V21
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             DataService ds = new DataService();
 
             Console.Title = "Спринт #3 | Выполнил: Чугин Н. М. | АСОиУб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #3                                                               *");
-            Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
-            Console.WriteLine("* Задание #2                                                              *");
-            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
             Console.WriteLine("* Выполнил: Чугин Никита Максимович | АСОиУб-25-1                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, используя цикл do...while, которая вычисляет        *");
-            Console.WriteLine("* произведение ряда по формуле.                                           *");
+            Console.WriteLine("* Используя цикл foreach заменить цифры на букву e в строке               *");
+            Console.WriteLine("*                                                                         *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Нет.                                                                    *");
+            Console.WriteLine("* f3g5ht g4j 34kg4                                                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine(Math.Round(ds.GetSumSeries(1, 10), 3));
+            string value = "f3g5ht g4j 34kg4";
+            Console.WriteLine(ds.ReplaceNumOnChar(value, 'e'));
         }
     }
 }
